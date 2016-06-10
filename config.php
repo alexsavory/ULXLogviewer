a610202623c1913406df908b26cb8ee066ac3a73
Alex <trurascalz@users.noreply.github.com>
2016-06-10 22:44:04 +0100

    Update config.php
    missing a ";"

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $host = "127.0.0.1";
 $user = "tru";
 $password = "password";
 $port = "21"; // Default 21, some hosts have different
-$passiveftp = "0" // 1 = Yes , 0 = No, Default 0. Try enabling this if it fails to show directory
+$passiveftp = "0"; // 1 = Yes , 0 = No, Default 0. Try enabling this if it fails to show directory
 // Log Directory
 $path = '/ulx_logs/';
 
